4fdaf83260adc46598f6ab96a7471e5c22af9310
RobertSajdak <robert.sajdak@onet.eu>
2023-01-19 00:29:01 +0100

    Dodaj pliki projektów.

---
+++ Day_4_Case/Program.cs
@@ -0,0 +1,29 @@
+﻿var id = 1;
+var name = "Ewa";
+var age = 33;
+char sex = 'K'; // K - kobieta, M - mężczyzna
+
+if (sex == 'K' && age >=1 && age < 30)
+{
+    Console.WriteLine("\nJesteś kobietą w wieku poniżej 30 lat.");
+}
+
+else if (sex == 'K' && age > 30)
+{
+    Console.WriteLine($"\nJesteś kobietą po 30-tce.");
+}
+
+else if (name == "Ewa" && sex == 'K' && age == 33)
+{
+    Console.WriteLine($"\n{name}, lat {age}.");
+}
+
+else if (sex != 'K' && age >=1 && age < 18)
+{
+    Console.WriteLine("\nJesteś niepełnoletnim mężczyzną.");
+}
+
+else
+{
+    Console.WriteLine("\nJestaś dorosłym facetem!");
+}
