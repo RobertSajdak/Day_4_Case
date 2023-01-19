23ddd2261b93ff20995da2b6000e2536684b56e7
RobertSajdak <robert.sajdak@onet.eu>
2023-01-19 11:32:32 +0100

    Edycja i skrócenie kodu.

--- Day_4_Case/Program.cs
+++ Day_4_Case/Program.cs
@@ -3,27 +3,17 @@ var name = "Ewa";
 var age = 33;
 char sex = 'K'; // K - kobieta, M - mężczyzna
 
-if (sex == 'K' && age >=1 && age < 30)
+if (sex == 'K') 
 {
-    Console.WriteLine("\nJesteś kobietą w wieku poniżej 30 lat.");
+    if (age >= 0 && age < 30)
+        Console.WriteLine("\nJesteś kobietą w wieku poniżej 30 lat.");
+    else if (age == 33 && name == "Ewa")
+        Console.WriteLine($"\n{name}, lat {age}.");
+    else if (age > 30)
+        Console.WriteLine($"\nJesteś kobietą po 30-tce.");
 }
 
-else if (sex == 'K' && age > 30)
-{
-    Console.WriteLine($"\nJesteś kobietą po 30-tce.");
-}
-
-else if (name == "Ewa" && sex == 'K' && age == 33)
-{
-    Console.WriteLine($"\n{name}, lat {age}.");
-}
-
-else if (sex != 'K' && age >=1 && age < 18)
-{
+else if (sex == 'M' && age >= 1 && age < 18)
     Console.WriteLine("\nJesteś niepełnoletnim mężczyzną.");
-}
-
 else
-{
     Console.WriteLine("\nJestaś dorosłym facetem!");
-}
